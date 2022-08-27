ffa0d2d244be872a141a7f1dc785de0ee69c4778
xose12 <xosekarikari@gmail.com>
2022-08-27 17:00:35 +0000

    message

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+    session_start();
+
+    $link = mysqli_connect("localhost", "root", "", "twitterclone");
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+   include("functions.php");
+   include("views/header.php");
+   include("views/home.php");
+   include("views/footer.php");
+
+   
+
+
+
+?>
